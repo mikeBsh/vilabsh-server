76bd5a26ab6e3666ecdade8acdfc1db7a109ed4c
Mike Busho <bushomichael@gmail.com>
2018-05-07 00:28:06 +0300

    example6

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express= require('express');
-var locale = require('browser-locale')();
+var getBrowserLanguage = require('get-browser-language');
 
 const port = process.env.PORT || 3000;
 var app= express();
 
-
+var lang = getBrowserLanguage()
 
 // app.use(express.static(__dirname+ '/public'));
 
 app.get('/', (req, res)=>{
 
-	res.send(locale);
+	res.send(lang);
 });
 
 app.listen(port, () =>{
